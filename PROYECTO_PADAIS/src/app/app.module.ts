e4d9a4447eaa4cda61c50bbd867d24657f57f12e
PabloNavarro712 <7501@itescam.edu.mx>
2023-09-25 02:26:54 -0600

    FAQ de la pagina

--- PROYECTO_PADAIS/src/app/app.module.ts
+++ PROYECTO_PADAIS/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './modules/home/home.module';
+import { FAQComponent } from './modules/faq/faq.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FAQComponent
   ],
   imports: [
     BrowserModule,
