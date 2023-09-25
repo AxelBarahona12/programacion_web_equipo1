86ac465e4ee2231e4cfc0b9ff456230d469d42e8
Fernando Yahir Puc Euan <80002186+FernandoPuc128@users.noreply.github.com>
2023-09-25 11:19:34 -0600

    Se agregó el horario de atención de la tienda

--- PROYECTO_PADAIS/src/app/modules/home/menu/menu.component.ts
+++ PROYECTO_PADAIS/src/app/modules/home/menu/menu.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+  daysOfWeek: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo'];
+  hoursOfDay: string[] = ['8:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '5:00 PM'];
+  schedule: any[][] = [];  
 
+  constructor() {
+    // Inicializa el horario de atención con valores predeterminados
+    for (let i = 0; i < this.daysOfWeek.length; i++) {
+      this.schedule[i] = [];
+      for (let j = 0; j < this.hoursOfDay.length; j++) {
+        this.schedule[i][j] = { isOpen: true };
+      }
+    } 
+    
+    // Establece que no se trabaja los domingos
+    const domingoIndex = this.daysOfWeek.indexOf('Domingo');
+    if (domingoIndex !== -1) {
+      for (let j = 0; j < this.hoursOfDay.length; j++) {
+        this.schedule[domingoIndex][j].isOpen = false;
+      }
+    }
+  }
 }
