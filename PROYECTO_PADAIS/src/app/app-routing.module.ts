df93933276fbd8bbbc066e1fd28c0ab3f7f54694
jesus7294 <138540870+JesusBs2@users.noreply.github.com>
2023-10-21 22:06:13 -0600

    Productos actulizados

--- PROYECTO_PADAIS/src/app/app-routing.module.ts
+++ PROYECTO_PADAIS/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeModule } from './modules/home/home.module';
-import { ProductosComponent } from './modules/Productos/Productos.component';
+import { ProductosComponent } from './Productos/Productos.component';
 const routes: Routes = [
   
   { path: "home", loadChildren:()=>import("./modules/home/home.module").then(m=>m.HomeModule)},
