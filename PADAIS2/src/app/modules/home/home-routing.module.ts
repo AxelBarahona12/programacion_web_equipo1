c146100ad8bf9ecfad3edf497da2efcc3e6b9d6d
AxelBarahona12 <AxelBarahona1202@outlook.com>
2023-10-24 10:20:31 -0600

    nuevoproyecto

---
+++ PADAIS2/src/app/modules/home/home-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: "", component: HomeComponent,
+    children: [
+    {path:"shoopcart", loadChildren:()=>import("../principal/principal.module").then(m=>m.PrincipalModule)},
+    {path:"products", loadChildren:()=>import("../products/products.module").then(m=>m.ProductsModule)}
+  ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
---
+++ PADAIS2/src/app/modules/principal/principal-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ShoopcartComponent } from './shoopcart/shoopcart.component';
+
+const routes: Routes = [
+ {path: "", component: ShoopcartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PrincipalRoutingModule { }
