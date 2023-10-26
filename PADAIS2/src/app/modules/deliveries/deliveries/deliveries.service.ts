9105205c21be1cf8f78e7630af458b457dc7e32f
Wicho <7257@itescam.edu.mx>
2023-10-26 01:15:53 -0600

    deliveries_color

--- PADAIS2/src/app/modules/deliveries/deliveries/deliveries.service.ts
+++ PADAIS2/src/app/modules/deliveries/deliveries/deliveries.service.ts
@@ -6,8 +6,15 @@ import { Injectable } from '@angular/core';
 export class DeliveriesService {
   private listaEntregas = [
     {
+      id_paquete: '0000003',
+      estado: 'Pago Pendiente',
+      direccionEntrega: 'Calle 26 #1',
+      fechaCompra: '27-10-2023',
+      fechaEntrega: '02-11-2023'
+    },
+    {
       id_paquete: '0000002',
-      estado: 'En tránsito',
+      estado: 'En Tránsito',
       direccionEntrega: 'Calle Principal #123',
       fechaCompra: '21-10-2023',
       fechaEntrega: '31-10-2023'
