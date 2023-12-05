6bbc17ea0563821239dcc1a622c36b988fe910ea
Wicho <7257@itescam.edu.mx>
2023-12-05 15:03:24 -0600

    Deliveries

--- PADAIS2/src/app/app.module.ts
+++ PADAIS2/src/app/app.module.ts
@@ -21,6 +21,8 @@ import { UserComponent } from './components/user/user.component';
 import { AdmRegisterComponent } from './components/adm-register/adm-register.component';
 import { AdmUpdateUsersComponent } from './components/adm-update-users/adm-update-users.component';
 import { UserdataComponent } from './components/userdata/userdata.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 
 @NgModule({
@@ -36,6 +38,8 @@ import { UserdataComponent } from './components/userdata/userdata.component';
     AdmRegisterComponent,
     AdmUpdateUsersComponent,
     UserdataComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
 
   ],
   imports: [
